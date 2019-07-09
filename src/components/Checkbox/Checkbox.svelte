4825702ace7f4dc7be3590c5e7820355be4fb68c
Adrien Peisl <adrien.peisl@liip.ch>
2019-07-09 16:17:12 +0200

    Prettify checkbox

--- src/components/Checkbox/Checkbox.svelte
+++ src/components/Checkbox/Checkbox.svelte
@@ -2,28 +2,26 @@
   export let checked = false;
   export let label = "";
   export let id = "checkbox" + Math.round(Math.random() * 95002000);
-
-  $: checkedClass = checked ? "checked" : "";
-  $: checkboxClasses = `${checkedClass}`;
 </script>
 
 <style type="text/scss">
   .checkbox {
+    --text-color: rgba(0, 0, 0, 0.75);
+    margin: 14px;
+
     * {
       cursor: pointer;
     }
 
-    margin: 14px;
-    --text-color: rgba(0, 0, 0, 0.75);
-
-    .box {
+    &__marker {
       width: 16px;
       height: 16px;
       border-radius: 3px;
       background: transparent;
       border: 2px solid grey;
     }
-    .input {
+
+    &__input {
       border: 0;
       clip: rect(0 0 0 0);
       height: 1px;
@@ -36,35 +34,26 @@
       -webkit-appearance: none;
     }
 
-    .field {
-      display: inline-flex;
-    }
-    .label {
+    &__label {
       user-select: none;
       margin-left: 12px;
       font-size: 14px;
       color: var(--text-color);
     }
-  }
 
-  /**
-  * Checked
-  */
-  .checkbox.checked {
-    .box {
-      background: #ddd;
+    &:checked {
+      // Checked state
+      .checkbox__marker {
+        background: #ddd;
+      }
     }
   }
 </style>
 
-<div class={'checkbox ' + checkboxClasses}>
-  <label class="field" for={id}>
-    <input {id} class="input" type="checkbox" bind:checked />
-    <div class="box">
-      <div class="checker" />
-    </div>
-    {#if label}
-      <div class="label">{label}</div>
-    {/if}
-  </label>
-</div>
+<label class="checkbox" for={id}>
+  <input {id} class="checkbox__input" type="checkbox" bind:checked />
+  <div class="checkbox__marker" />
+  {#if label}
+    <div class="checkbox__label">{label}</div>
+  {/if}
+</label>
